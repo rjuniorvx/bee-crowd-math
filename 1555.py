576568a27ecdda7287037eaacd0e1f20694a7387
Roberto Junior <167819423+rjuniorvx@users.noreply.github.com>
2025-05-16 20:18:07 -0300

    1555-commit

---
+++ 1555.py
@@ -0,0 +1,15 @@
+i = int(input())
+
+for _ in range(i):
+    x, y = map(int, input().split())
+    
+    r = ((3*x)**2) + y**2
+    b = 2*(x**2) + ((5*y)**2)
+    c = -100 * x + y ** 3
+
+    if r > b and r > c:
+        print("Rafael ganhou")
+    elif b > r and b > c:
+        print("Beto ganhou")
+    else:
+        print("Carlos ganhou")
